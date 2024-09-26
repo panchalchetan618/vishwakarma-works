31f3dcf2fb782041210b0676d7e7655361dd8b0b
Chetan Panchal <panchalchetan315@gmail.com>
2024-09-26 07:46:28 +0530

    first_commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./copmonents/home";
+
+function App() {
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ src/copmonents/home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section id="hero">
+            <h1 className="text-4xl">Welcome to my website!</h1>
+            <p>This is a simple website created using React and Vite.</p>
+            <a href="#about">About Me</a>
+        </section>
+    );
+};
+
+export default Home;
